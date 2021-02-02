37e635ff6132f5d31dc87bbdb6e3fb73166b7cb5
Cengizhan Ziyaeddin <c.zn1k96@gmail.com>
2021-02-02 16:13:56 +0000

    api router created

--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const errorHandler = require('errorhandler');
 const morgan = require('morgan');
 
 
+
+
 //initialise an instance of an express app
 const app = express ();
 
 //create a port number for the server to listen to.
 const PORT = process.env.PORT || 4001
 
+//mount packages
 app.use(bodyParser.json());
 app.use(cors());
 app.use(errorHandler());
 app.use(morgan('dev'));
+
+//Require api router from api.js and mount routes 
+const apiRouter = require('./api/api.js');
+app.use('/api', apiRouter);
 
 
 app.listen(PORT, () => {
